4ec9bb6b531613b4be372130064bfee6091e3e75
Michael <lovibanna30let@gmail.com>
2025-10-27 21:44:32 +0700

    create Main function

--- MainForm.cpp
+++ MainForm.cpp
@@ -1,2 +1,12 @@
 #include "MainForm.h"
 
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThreadAttribute]
+int main(array<String^>^ args) {
+	Application::SetCompatibleTextRenderingDefault(false);
+	Application::EnableVisualStyles();
+	ChinarDesktop::MainForm form;
+	Application::Run(% form);
+}
